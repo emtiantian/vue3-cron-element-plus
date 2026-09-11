6836642e8c5ad27cb9b69c2bba2300d7dd5c559b
your_name <hao503106@163.com>
2026-09-12 02:28:17 +0800

    feat: prepare first npm release

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ root: 'playground', base: './', plugins: [vue()] })
+export default defineConfig({
+  root: 'playground',
+  base: './',
+  build: { outDir: '../playground/dist', emptyOutDir: true },
+  plugins: [vue()],
+})
